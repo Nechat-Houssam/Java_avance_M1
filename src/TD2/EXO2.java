a2195d89b8d564916f466cdef59c507d23296273
Nechat-Houssam <houssam.nechat@dauphine.eu>
2022-09-25 22:38:12 +0200

    avance5

---
+++ src/TD2/EXO2.java
@@ -0,0 +1,43 @@
+package TD2;
+
+public class EXO2 {
+
+}
+
+/*
+
+1)
+  les erreurs sont:
+   
+   mere.miage(), miage nexiste pas pour la classe Mere
+   
+   h() : le type de retour dois etre le meme ce next pas le cas
+   i() : meme chose ici , la mere ne retourne rien tandis que la fille a un retour
+   e() : car on est passer de protected a private ce nest pas possible
+
+  
+ 2) la surcharge correpond a une methode de meme nom mais
+ de parametres differents;
+ la redefinisiion correpond a reprendre la meme methode mais
+ de changer son contenu , (methode fille differente de methode mere)
+ 
+  
+  refefinition:
+  
+  les 
+      
+      j() et l() t m ()pas le meme thorws dexeption  
+      a()
+      b()
+      c()
+      g() le retour nest pas le meme
+      
+    surcharge:
+      
+      le d() dans fille (pas meme parametres)
+      les c() dans mere et les c() dans fille
+      
+      
+      
+   
+*/
